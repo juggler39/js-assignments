67abd1515ecb2729debcf10a69945c154bda0c80
juggler39 <juggler39@yandex.ru>
2020-07-15 23:16:09 +0200

    improve FizzBuzz

--- task/06-conditions-n-loops-tasks.js
+++ task/06-conditions-n-loops-tasks.js
@@ -30,18 +30,7 @@
  *
  */
 function getFizzBuzz(num) {
-    let result = '';
-    if(num % 3 === 0) {
-        result = 'Fizz';
-    }
-    if(num % 5 === 0) {
-        result += 'Buzz';
-    }
-    if(result === '') {
-        result = num;
-    }
-
-    return result;
+    return (num % 3 ? '' : 'Fizz') + (num % 5 ? '' : 'Buzz') || num;
 }
 
 
